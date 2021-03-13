707e759088744c646c230ac2be43ea4c8208e2ed
matthewstclaire <matthew.crnich@gmail.com>
2021-03-12 17:07:38 -0700

    commit tree 27b87bb90fe7e90d4233e3b31328983a2b519657

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,15 @@
 export default (state = [], action) => {
-  return state;
-}
+  let idx;
+
+  switch (action.type) {
+    case 'ADD_QUOTE':
+      idx = state.quotes.findIndex((quote) => quote.id === action.id);
+      return {
+        ...state,
+        quotes: [...state.quotes.slice(0, idx), ...state.quotes.slice(idx + 1)],
+      };
+
+    default:
+      return state;
+  }
+};
